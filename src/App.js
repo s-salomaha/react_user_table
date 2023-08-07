2fa1fa8d61f0d3ba8bd0c4a651e7182bdef1d63d
s-salomaha <s.salomaha@gmail.com>
2023-08-07 09:51:37 +0300

    Add table and submit button

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import './App.css';
 import { Navbar } from "./components/Navbar";
+import { Table } from "./components/Table";
 
 function App() {
   return (
@@ -8,6 +9,8 @@ function App() {
       <Navbar />
       <div className="container pt-4">
         <h1 className="mb-5">Users:</h1>
+        <Table />
+        <button type="button" className="btn btn-primary mt-4">Add new user</button>
       </div>
     </>
   );
